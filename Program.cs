78e128ba420c36343052aa05b940c74d1c0541c3
Michael Robards <mikerobards@icloud.com>
2021-10-04 15:02:04 -0400

    learn strings

--- Program.cs
+++ Program.cs
@@ -6,75 +6,96 @@ namespace MikesPieShopHRM
     {
         static void Main(string[] args)
         {
-            int monthlyWage = 1234;
-            int months = 12, bonus = 1000;
+            string firstName = "Bethany";
+            string lastName = "Smith";
 
-            bool isActive = true;
-            double rating = 99.25;
+            string fullName = "Bethany Smith";
 
-            double ratePerHour = 12.34;
-            int numberOfHoursWorked = 165;
+            string noValueString = null;
+            string s;
 
-            long veryLongMonth = numberOfHoursWorked;
-            double d = 123456789.0;
-            int x = (int) d;
+            s = firstName;
 
-            int intVeryLongMonth = (int) veryLongMonth;
-
-            double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
-
-            Console.WriteLine(currentMonthWage);
-
-            ratePerHour += 3;
-
-            if(currentMonthWage > 2000)
-            {
-                //Console.WriteLine("Top paid employee");
-            }
-
-            int numberOfEmployees = 15;
-            numberOfEmployees--;
-
-            int intMaxValue = int.MaxValue;
-            int intMinValue = int.MinValue;
-
-            char userSelection = 'a';
-            char upperVersion = char.ToUpper(userSelection);
-            bool isDigit = char.IsDigit(userSelection);
-            bool isLetter = char.IsLetter(userSelection);
-
-            DateTime someDateTime = new DateTime(2021, 03, 28);
-            DateTime today = DateTime.Today;
-            DateTime twoDaysLater = someDateTime.AddDays(2);
-            DayOfWeek day = someDateTime.DayOfWeek;
-            bool isDST = someDateTime.IsDaylightSavingTime();
-            //Console.WriteLine(today);
-
-            DateTime hireDate = new DateTime(2021, 3, 28, 14, 30, 0);
-
-            //Console.WriteLine(hireDate);
-
-            DateTime exitDate = new DateTime(2021, 12, 11);
-
-            //DateTime invalidDate = new DateTime(2021, 15, 11);
-
-            DateTime startDate = hireDate.AddDays(15);
-
-            DateTime currentDate = DateTime.Now;
-            bool areWeInDst = currentDate.IsDaylightSavingTime();
-
-            DateTime startHour = DateTime.Now;
-            TimeSpan workTime = new TimeSpan(8, 35, 0);
-            DateTime endHour = startHour.Add(workTime);
-
-            Console.WriteLine(startHour.ToLongDateString());
-            Console.WriteLine(endHour.ToShortTimeString());
-
+            var userName = "BethanyS";
 
 
 
 
             Console.ReadLine();
         }
+
+
+        //static void Main(string[] args)
+        //{
+        //    var monthlyWage = 1234;
+        //    int months = 12, bonus = 1000;
+
+        //    var isActive = true;
+        //    var rating = 99.25;
+
+        //    double ratePerHour = 12.34;
+        //    int numberOfHoursWorked = 165;
+
+        //    long veryLongMonth = numberOfHoursWorked;
+        //    double d = 123456789.0;
+        //    int x = (int) d;
+
+        //    int intVeryLongMonth = (int) veryLongMonth;
+
+        //    double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
+
+        //    //Console.WriteLine(currentMonthWage);
+
+        //    ratePerHour += 3;
+
+        //    if(currentMonthWage > 2000)
+        //    {
+        //        //Console.WriteLine("Top paid employee");
+        //    }
+
+        //    int numberOfEmployees = 15;
+        //    numberOfEmployees--;
+
+        //    int intMaxValue = int.MaxValue;
+        //    int intMinValue = int.MinValue;
+
+        //    char userSelection = 'a';
+        //    char upperVersion = char.ToUpper(userSelection);
+        //    bool isDigit = char.IsDigit(userSelection);
+        //    bool isLetter = char.IsLetter(userSelection);
+
+        //    DateTime someDateTime = new DateTime(2021, 03, 28);
+        //    DateTime today = DateTime.Today;
+        //    DateTime twoDaysLater = someDateTime.AddDays(2);
+        //    DayOfWeek day = someDateTime.DayOfWeek;
+        //    bool isDST = someDateTime.IsDaylightSavingTime();
+        //    //Console.WriteLine(today);
+
+        //    DateTime hireDate = new DateTime(2021, 3, 28, 14, 30, 0);
+
+        //    //Console.WriteLine(hireDate);
+
+        //    DateTime exitDate = new DateTime(2021, 12, 11);
+
+        //    //DateTime invalidDate = new DateTime(2021, 15, 11);
+
+        //    DateTime startDate = hireDate.AddDays(15);
+
+        //    DateTime currentDate = DateTime.Now;
+        //    bool areWeInDst = currentDate.IsDaylightSavingTime();
+
+        //    DateTime startHour = DateTime.Now;
+        //    TimeSpan workTime = new TimeSpan(8, 35, 0);
+        //    DateTime endHour = startHour.Add(workTime);
+
+        //    Console.WriteLine(startHour.ToLongDateString());
+        //    Console.WriteLine(endHour.ToShortTimeString());
+
+
+
+
+
+        //    Console.ReadLine();
+        //}
     }
 }

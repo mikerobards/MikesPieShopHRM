a9e3c1c13ee3f47b95ed72a2ccab23fc3a3d39f8
Michael Robards <mikerobards@icloud.com>
2021-10-04 14:22:36 -0400

    using types

--- Program.cs
+++ Program.cs
@@ -15,6 +15,12 @@ namespace MikesPieShopHRM
             double ratePerHour = 12.34;
             int numberOfHoursWorked = 165;
 
+            long veryLongMonth = numberOfHoursWorked;
+            double d = 123456789.0;
+            int x = (int) d;
+
+            int intVeryLongMonth = (int) veryLongMonth;
+
             double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
 
             Console.WriteLine(currentMonthWage);
@@ -23,7 +29,7 @@ namespace MikesPieShopHRM
 
             if(currentMonthWage > 2000)
             {
-                Console.WriteLine("Top paid employee");
+                //Console.WriteLine("Top paid employee");
             }
 
             int numberOfEmployees = 15;
@@ -42,7 +48,29 @@ namespace MikesPieShopHRM
             DateTime twoDaysLater = someDateTime.AddDays(2);
             DayOfWeek day = someDateTime.DayOfWeek;
             bool isDST = someDateTime.IsDaylightSavingTime();
-            Console.WriteLine(today);
+            //Console.WriteLine(today);
+
+            DateTime hireDate = new DateTime(2021, 3, 28, 14, 30, 0);
+
+            //Console.WriteLine(hireDate);
+
+            DateTime exitDate = new DateTime(2021, 12, 11);
+
+            //DateTime invalidDate = new DateTime(2021, 15, 11);
+
+            DateTime startDate = hireDate.AddDays(15);
+
+            DateTime currentDate = DateTime.Now;
+            bool areWeInDst = currentDate.IsDaylightSavingTime();
+
+            DateTime startHour = DateTime.Now;
+            TimeSpan workTime = new TimeSpan(8, 35, 0);
+            DateTime endHour = startHour.Add(workTime);
+
+            Console.WriteLine(startHour.ToLongDateString());
+            Console.WriteLine(endHour.ToShortTimeString());
+
+
 
 
 
